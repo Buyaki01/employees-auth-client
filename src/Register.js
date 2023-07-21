640812aa5d1e9c8f55892ce815a4db7c20d5621e
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-21 14:30:47 +0300

    Add input field for username

--- src/Register.js
+++ src/Register.js
@@ -13,7 +13,64 @@ const Register = () => {
   const userRef = useRef()
   const errRef = useRef()
 
+  const [user, setUser] = useState('')
+  const [validName, setValidName] = useState(false)
+  const [userFocus, setUserFocus] = useState(false)
+
+  const [pwd, setPwd] = useState('')
+  const [validPwd, setValidPwd] = useState(false)
+  const [pwdFocus, setPwdFocus] = useState(false)
+
+  const [matchPwd, setMatchPwd] = useState('')
+  const [validMatch, setValidMatch] = useState(false)
+  const [matchFocus, setMatchFocus] = useState(false)
+
+  const [errMsg, setErrMsg] = useState('')
+  const [success, setSuccess] = useState(false)
+
+  useEffect(() => {
+    userRef.current.focus()
+  }, [])
+
+  useEffect(() => {
+    const result = USER_REGEX.test(user)
+    setValidName(result)
+  }, [user])
+
+  useEffect(() => {
+    const result = PWD_REGEX.test(pwd)
+    setValidPwd(result)
+    const match = pwd === matchPwd
+    setValidMatch(match)
+  }, [pwd, matchPwd])
+
+  useEffect(() => {
+    setErrMsg('')
+  }, [user, pwd, matchPwd])
+
   return (
-    <div></div>
+    <section>
+      <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">
+        {errMsg}
+      </p>
+
+      <h1>Register</h1>
+
+      <form>
+        <label htmlFor="username">Username: </label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          autoComplete="off"
+          onChange={(e) => setUser(e.target.value)}
+          required
+          aria-invalid={validName ? "false" : "true"}
+          aria-describedby="uidnote"
+          onFocus={() => setUserFocus(true)}
+          onBlur={() => setUserFocus(false)}
+        />
+      </form>
+    </section>
   )
 }
