7b44e4c830be7791471a5912709ffa9602dff36c
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-25 08:16:21 +0300

    Add Login component with username input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import Register from "./Register"
+import Login from "./Login"
+// import Register from "./Register"
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Login />
     </div>
   )
 }
