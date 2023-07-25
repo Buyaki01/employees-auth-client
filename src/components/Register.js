dbbb9d3f2cd9f0f34acaf986d6ec1e2cd39ec46a
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-25 13:17:52 +0300

    Install react-router-dom

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,7 @@
 import { useRef, useState, useEffect } from "react"
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import axios from "./api/axios"
+import axios from "../api/axios"
 
 //requires a string that starts with a letter, followed by a combination of letters, digits, hyphens, and underscores, with a total length between 3 and 23 characters
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/ 
