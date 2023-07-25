987e7b70b41295dfce4a3c2f2ad1f12785166db6
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-25 15:03:19 +0300

    Add Home component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Register from "./components/Register"
 import Layout from "./components/Layout"
 import LinkPage from "./components/LinkPage"
 import Unauthorized from "./components/Unauthorized"
+import Home from "./components/Home"
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Route path="register" element={<Register />} />
         <Route path="linkpage" element={<LinkPage />} />
         <Route path="unauthorized" element={<Unauthorized/>} />
+
+        <Route path="/" element={<Home />} />
       </Route>
     </Routes>
   )
