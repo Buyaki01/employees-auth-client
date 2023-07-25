c6a10ee1c7d1e8685c014411d297ff9657ea66c4
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-25 08:39:32 +0300

    Add handleSubmit method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Login from "./Login"
-// import Register from "./Register"
+import Register from "./Register"
 
 function App() {
   return (
     <div className="App">
+      <Register />
       <Login />
     </div>
   )
--- src/Login.js
+++ src/Login.js
@@ -17,40 +17,67 @@ const Login = () => {
     setErrMsg('')
   }, [user, pwd])
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    
+  }
+
   return (
-    <section>
-      <p 
-        ref={errRef}
-        className={errMsg ? "errmsg" : "offscreen"}
-        aria-live="assertive"
-      >
-        {errMsg}
-      </p>
+    <>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <a href="#">Go to Home</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p 
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
 
-      <h1>Sign In</h1>
+          <h1>Sign In</h1>
 
-      <form>
-        <label htmlFor="username">Username:</label>
-        <input 
-          type="text"
-          id="username"
-          ref={userRef}
-          autoComplete="off"
-          onChange={(e) => setUser(e.target.value)}
-          value={user}
-          required
-        />
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input 
+              type="text"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required
+            />
 
-       <label htmlFor="password">Password:</label>
-        <input 
-          type="password"
-          id="password"
-          onChange={(e) => setPwd(e.target.value)}
-          value={pwd}
-          required
-        />
-      </form>
-    </section>
+          <label htmlFor="password">Password:</label>
+            <input 
+              type="password"
+              id="password"
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+              required
+            />
+
+            <button>Sign In</button>
+          </form>
+
+          <p>
+            Need an Account? <br />
+            <span className="line">
+              <a href="#">Sign Up</a>
+            </span>
+          </p>
+        </section>
+      )
+      }
+    </>
   )
 }
 
