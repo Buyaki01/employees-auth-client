1825c408843e6d8e4e6eabaf89872c55295e7ea8
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-27 14:28:17 +0300

    fetch users from the backend in the Users component

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,33 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import axios from "../api/axios"
 
 const Users = () => {
   const [users, setUsers] = useState()
+
+  useEffect(() => {
+    let isMounted = true
+
+    const controller = new AbortController()
+
+    const getUsers = async () => {
+      try {
+        const response = await axios.get('/users', {
+          signal: controller.signal
+        })
+        isMounted && setUsers(response.data)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    getUsers()
+
+    return () => {
+      isMounted = false
+      controller.abort()
+    }
+
+  }, [])
 
   return (
     <article>
