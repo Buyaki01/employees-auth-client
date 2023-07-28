6185f4fe903c39ba5bb394af329bb2b1603b72e8
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-28 05:47:28 +0300

    Test whether the refresh token works when one clicks the refresh button

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react"
 import axios from "../api/axios"
+import useRefreshToken from "../hooks/useRefreshToken"
 
 const Users = () => {
   const [users, setUsers] = useState()
+
+  const refresh = useRefreshToken()
 
   useEffect(() => {
     let isMounted = true
@@ -43,6 +46,8 @@ const Users = () => {
         )
         : <p>No users to display</p>
       }
+      <button onClick={() => refresh()}>Refresh</button>
+      <br />
     </article>
   )
 }
