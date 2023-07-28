ff7c6d45bfe7df32300a8e7b1c5d89ec73d6a088
Buyaki01 <rittahbuyaki@gmail.com>
2023-07-28 06:52:48 +0300

    Add requestIntercept in the useAxiosPrivate hook

--- src/hooks/useAxiosPrivate.js
+++ src/hooks/useAxiosPrivate.js
@@ -8,6 +8,15 @@ const useAxiosPrivate = () => {
   const { auth } = useAuth()
 
   useEffect(() => {
+
+    const requestIntercept = axiosPrivate.interceptors.request.use(
+      config => {
+        if (!config.headers['Authorization']) {
+          config.headers['Authorization'] = `Bearer ${auth?.accessToken}`
+        }
+        return config
+      }, (error) => Promise.reject(error)
+    )
     
     const responseIntercept = axiosPrivate.interceptors.response.use(
       response => response,
@@ -19,8 +28,15 @@ const useAxiosPrivate = () => {
           prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`
           return axiosPrivate(prevRequest)
         }
+
+        return Promise.reject(error)
       }
     )
+
+    return () => {
+      axiosPrivate.interceptors.request.eject(requestIntercept)
+      axiosPrivate.interceptors.response.eject(responseIntercept)
+    }
 
   }, [auth, refresh])
 
