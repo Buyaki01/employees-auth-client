fc5a2f4dc3bd6eef55ad31249160c6a6a0ee3c8d
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-01 12:30:04 +0300

    Add to LoginAuth0 component

--- src/components/LoginAuth0.js
+++ src/components/LoginAuth0.js
@@ -1,7 +1,15 @@
-import { useAuth } from "@auth0/auth0-react"
+import { useAuth0 } from "@auth0/auth0-react"
 
 const LoginAuth0 = () => {
-   
+  const { loginWithRedirect, isAuthenticated } = useAuth0()
+
+  return (
+    !isAuthenticated && (
+      <button onClick={() => loginWithRedirect()}>
+        Sign In
+      </button>
+    )
+  )
 }
 
 export default LoginAuth0
