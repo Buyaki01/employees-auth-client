a30f01ad362aaa4ee754dcb046cc89f3f19b214b
Buyaki01 <rittahbuyaki@gmail.com>
2023-08-01 12:22:47 +0300

    Add Auth0Provider in index.js

---
+++ src/components/LoginAuth0.js
@@ -0,0 +1,7 @@
+import { useAuth } from "@auth0/auth0-react"
+
+const LoginAuth0 = () => {
+   
+}
+
+export default LoginAuth0
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App'
 import { AuthProvider } from './context/AuthProvider'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+import { Auth0Provider } from '@auth0/auth0-react'
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 if (process.env.NODE_ENV === 'production') {
   disableReactDevTools()
@@ -15,9 +19,15 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path='/*' element={<App />} />
-        </Routes>
+        <Auth0Provider
+          domain={domain}
+          clientId={clientId}
+          redirectUri={window.location.origin}
+        >
+          <Routes>
+            <Route path='/*' element={<App />} />
+          </Routes>
+        </Auth0Provider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
